a1190c0d16e74168beb60c5add9428ec17a2ec8c
m-m-e <megmyfedwards@gmail.com>
2019-06-17 11:16:57 +0200

    Add fetch to get list of pokemon and url to more info

--- src/components/PokedexApp/PokedexApp.js
+++ src/components/PokedexApp/PokedexApp.js
@@ -4,36 +4,70 @@ class PokedexApp extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      pokeData: [
-        {
-          "name": "bulbasaur",
-          "id": 1,
-          "picture-front": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
-          "picture-back": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png",
-          "types": [ 
-            {
-              "slot": 2,
-              "type": {
-                  "name": "poison",
-                  "url": "https://pokeapi.co/api/v2/type/4/"
-              }
-            },
-            {
-              "slot": 1,
-              "type": {
-                  "name": "grass",
-                  "url": "https://pokeapi.co/api/v2/type/12/"
-              }
-            }]
+      pokeList: [],
+      pokeData: {
+        name: {
+          "id": 0,
+          "picture-front": "",
+          "picture-back": "",
+          "types": []
         }
-      ]
+      }
     }
+
+    this.getPokemonList = this.getPokemonList.bind(this);
+    // this.getPokemonData = this.getPokemonData.bind(this);
   }
+
+  componentDidMount(){
+    this.getPokemonList();
+  }
+
+  getPokemonList(){
+    const endpoint = "https://pokeapi.co/api/v2/pokemon?limit=25/";
+    fetch(endpoint)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({pokeList: data.results});
+        fetch(data.results[0].url)
+          .then(response => response.json())
+          .then(data => {
+            const newPokemon = {
+              [data.name]: {
+                "id": data.id,
+                "picture-front": data.sprites.front_default,
+                "picture-back": data.sprites.back_default,
+                "types": data.types
+              }
+            };
+          this.setState({pokeData: newPokemon})
+        })
+      })
+      .catch(error => console.error(error))
+  }
+
+  
+    // for (let i = 0; i < 25; i++){
+    //   fetch(pokeList[i].url)
+    //     .then(response => response.json())
+    //     .then(data => {
+    //       newPokemon = {
+    //         [data.name]: {
+    //           "id": data.id,
+    //           "picture-front": data.sprites.front_default,
+    //           "picture-back": data.sprites.back_default,
+    //           "types": data.types
+    //         }
+    //       };
+    //       this.setState({pokeData: newPokemon})
+    //       // this.setState(prevState => ({pokeData: [...prevState.pokeData, newPokemon]}))
+    //     })
+
   render(){
-    const {pokeData} = this.state;
+    // const {pokeList} = this.state;
     return (
       <div className="PokedexApp">
-        <p>{pokeData[0].name}</p>
+        <p>Hola</p>
       </div>
     );
   }
